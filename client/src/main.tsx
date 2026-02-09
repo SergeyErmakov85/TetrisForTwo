f3d5873bea0315c82969fccda5b876ad70f7e37b
SergeyErmakov85 <45623987+SergeyErmakov85@users.noreply.github.com>
2026-02-09 15:11:22 +0300

    Updated main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,21 @@
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-// Simply render the app
 const rootElement = document.getElementById("root");
 
 if (rootElement) {
   console.log("Rendering app to root element");
   const root = createRoot(rootElement);
-  root.render(<App />);
+  root.render(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
 } else {
   console.error("Root element not found");
 }
